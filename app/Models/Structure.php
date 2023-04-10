070661ae00905111f6daf96c9858d354bb2aaf27
joshuajevon <joshuajevon05@gmail.com>
2023-04-10 09:24:19 +0700

    feat: add structure model and migration

---
+++ app/Models/Structure.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Structure extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'profile_photo',
+        'profile_name',
+        'profile_division',
+        'profile_sub_division',
+        'profile_position'
+    ];
+}
